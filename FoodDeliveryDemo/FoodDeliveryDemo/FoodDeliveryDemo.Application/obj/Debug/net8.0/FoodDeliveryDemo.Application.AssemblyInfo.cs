c79a9c5ab15ca5737a23a406c23a3234b9f2008d
Sahil-109 <vaghelasahil26@gmail.com>
2024-11-24 19:58:08 +0530

    commit --SV

--- FoodDeliveryDemo/FoodDeliveryDemo/FoodDeliveryDemo.Application/obj/Debug/net8.0/FoodDeliveryDemo.Application.AssemblyInfo.cs
+++ FoodDeliveryDemo/FoodDeliveryDemo/FoodDeliveryDemo.Application/obj/Debug/net8.0/FoodDeliveryDemo.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodDeliveryDemo.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04f4773942cc935334035dcc8df10962130b1688")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodDeliveryDemo.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodDeliveryDemo.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
